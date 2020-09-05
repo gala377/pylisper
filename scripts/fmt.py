7eeda3790be499fa0318c54f627f62d3e21dc235
Rafał Galczak <r.galczak@gmail.com>
2020-09-05 19:47:35 +0200

    Changed fmt script to run for test directory as well

--- scripts/fmt.py
+++ scripts/fmt.py
@@ -3,6 +3,7 @@ from subprocess import run
 
 def main():
     for dir in ['pylisper', 'tests']:
+        print(f"Running for {dir}")
         run(['black', dir])
         run(['isort', dir])
         run(['autoflake', '-i', '-r', '--ignore-init-module-imports', dir])
